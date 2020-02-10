df60d197aa3847f990eb469c098218442a5f5b28
wallacemkk <wallaceedua@gmail.com>
2020-02-10 11:49:27 -0300

    feat: Add Mail Config

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '503216e3da3e64',
+    pass: '910d71812914fc',
+  },
+  default: {
+    from: 'Equipe FasFeet <noreply@fastfeet.com>',
+  },
+};
